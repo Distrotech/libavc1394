a3ccdc107cf89e8f4d5d91cff5d276e5843338de
ddennedy <ddennedy@0137f130-5211-0410-8cb4-f8f0c3e9e8b0>
2005-01-25 23:43:12 +0000

    make build use libraw pkg-config; drop libplug1394 and related
    git-svn-id: svn://svn.code.sf.net/p/libavc1394/code/trunk@40 0137f130-5211-0410-8cb4-f8f0c3e9e8b0

--- libavc1394/common/raw1394util.c
+++ libavc1394/common/raw1394util.c
@@ -9,35 +9,14 @@ int cooked1394_read(raw1394handle_t handle, nodeid_t node, nodeaddr_t addr,
 {
     int retval, i;
 	struct timespec ts = {0, RETRY_DELAY};
-#ifdef RAW1394_V_0_8
-    int ackcode, rcode;
-#endif
     for(i=0; i<MAXTRIES; i++) {
         retval = raw1394_read(handle, node, addr, length, buffer);
-#ifdef RAW1394_V_0_9
         if (retval < 0 && errno == EAGAIN)
             nanosleep(&ts, NULL);
         else
             return retval;
     }
     return -1;
-#else
-        if( retval < 0 ) return retval;
-        ackcode = RAW1394_MASK_ACK(retval);
-        rcode = RAW1394_MASK_RCODE(retval);
-        if( ackcode == ACK_BUSY_X || ackcode == ACK_BUSY_A
-                || ackcode == ACK_BUSY_B ) {
-            nanosleep(&ts, NULL);
-        } else {
-            break;
-        }
-    }
-
-    if( ackcode != ACK_PENDING && ackcode != ACK_COMPLETE &&
-            ackcode != ACK_LOCAL)
-        return -1;
-    return retval;
-#endif
 }
 
 int cooked1394_write(raw1394handle_t handle, nodeid_t node, nodeaddr_t addr,
@@ -45,34 +24,12 @@ int cooked1394_write(raw1394handle_t handle, nodeid_t node, nodeaddr_t addr,
 {
     int retval, i;
 	struct timespec ts = {0, RETRY_DELAY};
-#ifdef RAW1394_V_0_8
-    int ackcode, rcode;
-#endif
     for(i=0; i<MAXTRIES; i++) {
         retval = raw1394_write(handle, node, addr, length, data);
-#ifdef RAW1394_V_0_9
         if (retval < 0 && errno == EAGAIN)
             nanosleep(&ts, NULL);
         else
             return retval;
     }
     return -1;
-#else
-        if(retval < 0 )
-            return retval;
-        ackcode = RAW1394_MASK_ACK(retval);
-        rcode = RAW1394_MASK_RCODE(retval);
-        if( ackcode == ACK_BUSY_X || ackcode == ACK_BUSY_A
-                || ackcode == ACK_BUSY_B ) {
-            nanosleep(&ts, NULL);
-        } else {
-            break;
-        }
-    }
-
-    if( ackcode != ACK_PENDING && ackcode != ACK_COMPLETE &&
-            ackcode != ACK_LOCAL)
-        return -1;
-    return retval;
-#endif
 }
